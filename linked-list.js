ca8f77d93513a9ec8336bcad1e972c47a61d3820
Akandra-Pawitra <akandrapawitra@gmail.com>
2022-12-23 19:02:56 +0700

    Create prepend() and #addNode() method

--- linked-list.js
+++ linked-list.js
@@ -8,6 +8,16 @@ class LinkedList {
     }
   }
 
+  #addNode (value) {
+    // create a new node
+    const newNode = new Node(value)
+    // store the node in memory
+    this.memory.list.push(newNode)
+    // create an address to attach to next pointer
+    const address = this.memory.list.indexOf(newNode)
+    return address
+  }
+
   #nextPointer (next, address) {
     /* technically pointer variable is unnecessary, but (pointer === null)
       is easier to read than (this.memory.list[next].next === null) */
@@ -20,17 +30,25 @@ class LinkedList {
   }
 
   append (value) {
-    // create a new node
-    const newNode = new Node(value)
-    // store the node in memory
-    this.memory.list.push(newNode)
-    // create an address to attach to next pointer
-    const address = this.memory.list.indexOf(newNode)
+    const address = this.#addNode(value)
     // if list is empty, attach address to head
     if (this.memory.list.length === 1) this.memory.head = address
     else {
       // recursively follow next pointer, start from head
       this.#nextPointer(this.memory.head, address)
+    }
+  }
+
+  prepend (value) {
+    const address = this.#addNode(value)
+    if (this.memory.list.length === 1) this.memory.head = address
+    else {
+      // copy head pointer
+      const pointer = this.memory.head
+      // point node next to head address
+      this.memory.list[address].next = pointer
+      // change head address to node address
+      this.memory.head = address
     }
   }
 }
@@ -43,7 +61,14 @@ class Node {
 }
 
 const LIST = new LinkedList()
-for (let i = 91; i <= 100; i++) {
-  LIST.append(i)
-}
+
+LIST.append(0)
+LIST.prepend(1)
+LIST.append(2)
+LIST.prepend(3)
+LIST.append(4)
+LIST.prepend(5)
+LIST.append(6)
+LIST.prepend(7)
+
 console.log(LIST.memory)
